a4562967346b0f99f442eea8742d4797d0b23db2
ssr2112 <107883000+ssr2112@users.noreply.github.com>
2023-08-05 21:32:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-const path = require("path");
+import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -31,13 +35,13 @@ app.use("/api/v1/product/", productRoutes);
 //static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
